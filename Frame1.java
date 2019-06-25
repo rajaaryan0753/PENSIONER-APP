69ab8df13cf8094c357cf05e739a83697ed0d580
Sairaj64.com <shetyesairaj@gmail.com>
2019-06-25 23:02:54 +0530

    made corrections

--- Frame1.java
+++ Frame1.java
@@ -1,17 +1,18 @@
 package javatestgui;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.sql.*;
-
-
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JSeparator;
+import javax.swing.ImageIcon;
 
 public class Frame1 {
 
@@ -51,26 +52,47 @@ public class Frame1 {
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnsubmit = new JButton("Submit");
+		/* CONNECTION CODE
 		btnsubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				//public class fetchdata { (ERROR : Not required to create a separate class I think)
+	
+				public static String connectionString = "jdbc:sap://<connection string>";
+				public static String user = "<user name>";
+				public static String password = "<password>";
+		
+				//public static void main(String[] argv) { (ERROR: 2 main functions in the file)
+				
+				if (Connection connection = null) {
 				try {
-					Class.forName("com.mysql.jdbc.Driver`");
-					Connection con=DriverManager.getConnection("jdbc:mysql:localhost:3306/test","root","");
-					Statement stmt=con.createStatement();
-					String sql="Select " from table where Pensionerid='"+textpensioner.getText()"';
-					ResultSet rs=stmt.executeQuery(sql);
-					if(rs.next())
-					{
-						JOptionPane.showMessageDialog(null, "Login Succesfull");
+				connection = DriverManager.getConnection(connectionString, user, password);
+				} catch (SQLException e) {
+				System.err.println("Connection Failed. User/Passwd Error? Message: " + e.getMessage());
+				return;
 					}
-					else
-			       {
-					JOptionPane.showMessageDialog(null, "Invalid Login Detail", "Login Error",JOptionPane.ERROR_MESSAGE );
-					textpensioner.setText(null);
-				    }
-				} catch (Exception e) {System.out.print(e);}
-			}
-		});
+				}
+				if (connection != null) {
+				try {
+					System.out.println("Connection to HANA successful!");
+					Statement stmt = connection.createStatement();
+					String sqlfetch = "select idno, name, des, lastdept, address, phoneno, retdate from dummy";
+					ResultSet rs = stmt.executeQuery(sqlfetch);
+					String id = rs.getString("idno");
+					String name = rs.getString("name");
+					String designation = rs.getString("des");
+					String lastdept = rs.getString("lastdept");
+					String address = rs.getString("address");
+					String phoneno = rs.getString("phoneno");
+					String retdate = rs.getString("retdate");
+					rs.close();
+					stmt.close();
+					connection.close();
+				} catch (SQLException e) {
+					System.err.println("Query failed!");
+					}
+				}
+		}
+});*/
 		btnsubmit.setBounds(309, 207, 114, 25);
 		frame.getContentPane().add(btnsubmit);
 		
@@ -84,15 +106,17 @@ public class Frame1 {
 		frame.getContentPane().add(btnreset);
 		
 		JLabel lblpensioner = new JLabel("PENSIONER LOGIN PAGE");
-		lblpensioner.setBounds(171, 12, 189, 15);
+		lblpensioner.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblpensioner.setBounds(126, 76, 189, 15);
 		frame.getContentPane().add(lblpensioner);
 		
-		JLabel lblPensionerId = new JLabel("PENSIONER ID");
-		lblPensionerId.setBounds(24, 78, 142, 34);
+		JLabel lblPensionerId = new JLabel("Pensioner ID: ");
+		lblPensionerId.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPensionerId.setBounds(87, 131, 102, 25);
 		frame.getContentPane().add(lblPensionerId);
 		
 		textpensioner = new JTextField();
-		textpensioner.setBounds(217, 86, 124, 19);
+		textpensioner.setBounds(191, 133, 141, 22);
 		frame.getContentPane().add(textpensioner);
 		textpensioner.setColumns(10);
 		
@@ -101,7 +125,16 @@ public class Frame1 {
 		frame.getContentPane().add(separator);
 		
 		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(24, 56, 399, 2);
+		separator_1.setBounds(24, 104, 399, 2);
 		frame.getContentPane().add(separator_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 434, 63);
+		frame.getContentPane().add(panel);
+		ImageIcon image = new ImageIcon("MPT.jpg");
+		JLabel label = new JLabel(image);
+		panel.add(label);
+		
+	
 	}
 }
